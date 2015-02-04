139bf550255e22ad73d27f5d567dac3990b4009d
Yuliya Kim <yk882@mail.missouri.edu>
2015-02-04 16:34:57 -0600

    added new files

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
+
+var playerData;
+
 $(document).ready(function() {
-    console.log("Hello world.")
+   getData();
 });
 
 
+function getData () {
+		$.getJSON("js/andre_ethier_card.json", function(data) {
+				playerData = data;
+				drawChart();
+		});
+}
+
+function drawChart() {
+		$.each(playerData.stats, function(i, item) 
+			{
+
+			var width = item.HR;
+				
+			$(".chart").append('<div class ="bar" style="width: '+width+'px">' + item.HR + '<div>');
 
 
+		});
+
+}
 
 
